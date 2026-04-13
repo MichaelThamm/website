b489b767810d095e544a3ee1c536da23494f5ecd
Michael Thamm <mike.thamm@canonical.com>
2026-04-12 21:13:32 -0400

    fix: heroku deploy

--- app/tests/test-urls.sh
+++ app/tests/test-urls.sh
@@ -9,7 +9,7 @@ links=$(grep -v 'preconnect\|preload\|dns-prefetch' ./app/templates/index.html \
 # Function to check if a given status code is acceptable
 is_acceptable_status() {
     case "$1" in
-        200|403|999) return 0 ;;
+        200|202|403) return 0 ;;
         *) return 1 ;;
     esac
 }
@@ -17,7 +17,7 @@ is_acceptable_status() {
 # Loop through each https:// link and check existence with curl
 for link in $links; do
     response=$(curl -sL -w "%{http_code}" "$link" -o /dev/null)
-    if is_acceptable_status "$response"; then
+    if is_acceptable_status "$response" || is_override_url "$link"; then
         echo "Link $link exists (HTTP $response)"
     else
         echo "Link $link does not exist or has an unacceptable status code (HTTP $response)"
